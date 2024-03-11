356e791b6b47ca0d76753436f1db0b8107e1c714
davidbacter01 <davidbacter01@gmail.com>
2024-03-11 19:30:19 +0200

    Add if example

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+let isChoreDone = false;
+let hasEaten = false;
+
+if (isChoreDone && hasEaten) {
+    console.log('Te poti uita la desene animate!');
+}
+
+if (!isChoreDone || !hasEaten) {
+
+    if (!isChoreDone) {
+        console.log('Inca mai ai de lucru! Poti sa te uiti la desene dupa ce ai terminat treaba.');
+    }
+
+    if (!hasEaten) {
+        console.log('Inca nu ai terminat de mancat! Te poti uita la desene cand farfuria e goala!');
+    }    
+}
